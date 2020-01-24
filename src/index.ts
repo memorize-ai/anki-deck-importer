ae83e4b9c4f544ea92195f958c1b214e43e6f55c
Ken Mueller <kenmueller0@gmail.com>
2020-01-23 20:33:31 -0800

    cleaned

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import downloadDeck from './download-deck'
-import importDeck from './import-deck'
-
-;(async () => {
-	const deckId = '162733352'
-	
-	await downloadDeck(deckId)
-	await importDeck(deckId, [])
-})().then(console.log).catch(console.error)
