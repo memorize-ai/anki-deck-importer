f2ac3d59f3664fdd51a4b308fb40c75f006624f2
Ken Mueller <kenmueller0@gmail.com>
2020-01-23 19:13:18 -0800

    readme

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # memorize.ai Anki deck importer
 
 **[memorize.ai](https://memorize.ai)**
+
+## `products/decks.json`
+
+```json
+{
+	"DECK_ID": {
+		"downloaded": true,
+		"imported": true,
+		"topics": [
+			"TOPIC_ID"
+		]
+	}
+}
+
+```
+
+## `products/topics.json`
+
+```json
+{
+	"TOPIC_ID": "TOPIC_NAME"
+}
+```
+
+## `products/decks/{DECK_ID}`
+
+```
+main.apkg
+```
--- src/constants.ts
+++ src/constants.ts
@@ -3,21 +3,12 @@ import { join } from 'path'
 export const ACCOUNT_ID = 'tte9RbI9akMzWfs1bWxJRz4nTou1'
 
 export const DECKS_PATH = join(__dirname, '../products/decks.json')
-
-export const DOWNLOADED_DECKS_PATH = join(__dirname, '../products/downloaded-decks.json')
+export const TOPICS_PATH = join(__dirname, '../products/topics.json')
 
 export const DECKS_DOWNLOAD_PATH = join(__dirname, '../products/decks')
 
-export const PAGE_WAIT_TIME = 1000
-
-export const BLACKLISTED_DECKS = [
-	'1910608929'
-]
+export const FIREBASE_ADMIN_KEY_PATH = join(__dirname, '../protected/firebase-admin.json')
 
 export const IMAGE_SRC_REGEX = /<img\s+[^>]*src\s*=\s*["'](.+?)["'][^>]*\/?>/
 
 export const SOUND_URL_REGEX = /\[\s*[Ss][Oo][Uu][Nn][Dd]\s*\:\s*(.+?)\s*\]/
-
-export const FIREBASE_ADMIN_KEY_PATH = join(__dirname, '../protected/firebase-admin.json')
-
-export const TOPICS_PATH = join(__dirname, '../products/topics.json')
