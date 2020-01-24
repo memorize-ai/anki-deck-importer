d34dadfcefb418605d2568f8cdbb17f8eecc2462
Ken Mueller <kenmueller0@gmail.com>
2020-01-23 20:27:40 -0800

    import-decks

---
+++ src/import-decks.ts
@@ -0,0 +1,28 @@
+import { writeFileSync as writeFile } from 'fs'
+
+import importDeck from './import-deck'
+import { DECKS_PATH } from './constants'
+
+const decks: Record<string, {
+	downloaded: boolean
+	imported: boolean
+	topics: string[]
+}> = require(DECKS_PATH)
+
+export default async () => {
+	for (const [deckId, deckData] of Object.entries(decks)) {
+		if (deckData.imported)
+			continue
+		
+		await importDeck(deckId, deckData.topics)
+		
+		deckData.imported = true
+		
+		writeFile(DECKS_PATH, JSON.stringify(decks))
+		
+		console.log(`Imported deck with ID ${deckId}`)
+	}
+}
+
+if (require.main === module)
+	exports.default().catch(console.error)
