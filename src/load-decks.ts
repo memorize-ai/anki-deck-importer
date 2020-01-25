fe7be313da217b7432cfd592176f2b0995cca766
Ken Mueller <kenmueller0@gmail.com>
2020-01-24 18:21:37 -0800

    load decks and topics

--- src/load-decks.ts
+++ src/load-decks.ts
@@ -9,36 +9,48 @@ const decks: Record<string, {
 	topics: string[]
 }> = require(DECKS_PATH)
 
-export default async (topics: Record<string, string[]> = require(TOPICS_PATH)) => {
-	for (const [topicId, topicNames] of Object.entries(topics))
-		for (const topicName of topicNames) {
-			const match = (await (await fetch(`https://ankiweb.net/shared/decks/${topicName}`)).text())
-				.match(/shared\.files\s=\s(\[.*\]?);/)
-			
-			if (!match) {
-				console.error(`Unable to load decks for topic ${topicId}: ${topicName}`)
-				continue
-			}
-			
-			const deckIds: string[] = JSON.parse(match[1])
-				.map(([deckId]: [number]) => deckId.toString())
-			
-			for (const deckId of deckIds)
-				addDeck(deckId, topicId)
+export default async (topics: Record<string, string[]> = loadTopics(require(TOPICS_PATH))) => {	
+	for (const [name, topicIds] of Object.entries(topics)) {
+		const match = (await (await fetch(`https://ankiweb.net/shared/decks/${name}`)).text())
+			.match(/shared\.files\s=\s(\[.*\]?);/)
+		
+		if (!match) {
+			console.error(`Unable to load decks for topic ${name}`)
+			continue
 		}
+		
+		const deckIds: string[] = JSON.parse(match[1])
+			.map(([deckId]: [number]) => deckId.toString())
+		
+		for (const deckId of deckIds)
+			addDeck(deckId, topicIds)
+		
+		delete topics[name]
+	}
 	
 	return topics
 }
 
-const addDeck = (deckId: string, topicId: string) => {
+const loadTopics = (topics: Record<string, string[]>) => {
+	const acc: Record<string, string[]> = {}
+	
+	for (const name of ([] as string[]).concat(...Object.values(topics)))
+		for (const [topicId, names] of Object.entries(topics))
+			if (names.includes(name))
+				acc[name] = [...acc[name] ?? [], topicId]
+	
+	return acc
+}
+
+const addDeck = (deckId: string, topicIds: string[]) => {
 	const existingDeck = decks[deckId]
 	
 	existingDeck
-		? existingDeck.topics = [...new Set([...existingDeck.topics, topicId])]
+		? existingDeck.topics = [...new Set([...existingDeck.topics, ...topicIds])]
 		: decks[deckId] = {
 			downloaded: false,
 			imported: false,
-			topics: [topicId]
+			topics: topicIds
 		}
 	
 	writeFile(DECKS_PATH, JSON.stringify(decks))
