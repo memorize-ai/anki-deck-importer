d0eb679de7a6f8ccdfb13ecefe5b7307b6b4452e
Ken Mueller <kenmueller0@gmail.com>
2020-01-29 19:54:06 -0800

    regex

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { join } from 'path'
 
-export const DEBUG = false
+export const DEBUG = false as boolean
 
 export const ACCOUNT_ID = DEBUG ? '5xh8ODFbtIfBh7LjA0zGRlpc3kf1' : 'LexVW5dWZHTQPe1DPPi68Zqt7aS2'
 
@@ -11,9 +11,9 @@ export const DECKS_DOWNLOAD_PATH = join(__dirname, '../products/decks')
 
 export const FIREBASE_ADMIN_KEY_PATH = join(__dirname, `../protected/firebase-admin${DEBUG ? '-dev' : ''}.json`)
 
-export const IMAGE_SRC_REGEX = /<img\s+[^>]*src\s*=\s*["'](.+?)["'][^>]*\/?>/
+export const IMAGE_SRC_REGEX = /<img.+?src\s*?=\s*?["'](.+?)["'].*?>/
 
-export const SOUND_URL_REGEX = /\[\s*[Ss][Oo][Uu][Nn][Dd]\s*\:\s*(.+?)\s*\]/
+export const SOUND_URL_REGEX = /\[\s*?[Ss][Oo][Uu][Nn][Dd]\s*?\:\s*?(.+?)\s*?\]/
 
 export const ASSET_CHUNK_SIZE = 500
 
