7d9b3c13fb286770e3f930f077135fd3bb0c4414
Ken Mueller <kenmueller0@gmail.com>
2020-01-29 19:49:12 -0800

    fixed account id

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import { join } from 'path'
 
 export const DEBUG = false
 
-export const ACCOUNT_ID = DEBUG ? '5xh8ODFbtIfBh7LjA0zGRlpc3kf1' : 'tte9RbI9akMzWfs1bWxJRz4nTou1'
+export const ACCOUNT_ID = DEBUG ? '5xh8ODFbtIfBh7LjA0zGRlpc3kf1' : 'LexVW5dWZHTQPe1DPPi68Zqt7aS2'
 
 export const DECKS_PATH = join(__dirname, '../products/decks.json')
 export const TOPICS_PATH = join(__dirname, '../products/topics.json')
